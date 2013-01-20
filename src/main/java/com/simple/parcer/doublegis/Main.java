b433ee2b2bb53173a68ad40c67b8a838fc1b0f8f
Kolipass <kolipass@gmail.com>
2013-01-20 14:48:22 +0700

    Добавлены задержки, чтобы не банился ключ при парсинге. Выпелил лишнее, оптимизировал

--- src/main/java/com/simple/parcer/doublegis/Main.java
+++ src/main/java/com/simple/parcer/doublegis/Main.java
@@ -1,6 +1,12 @@
 package com.simple.parcer.doublegis;
 
 import com.google.gson.Gson;
+import com.simple.parcer.doublegis.Models.DoubleGISFirmtModel;
+import com.simple.parcer.doublegis.Models.DoubleGISProjectModel;
+import com.simple.parcer.doublegis.Models.DoubleGISRubricModel;
+import com.simple.parcer.doublegis.ServerResponses.ProjectServerResponse;
+import com.simple.parcer.doublegis.ServerResponses.RubricsServerResponse;
+import com.simple.parcer.doublegis.ServerResponses.SearchRubricsServerResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -13,12 +19,39 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <OL><DT><B> Содержит точку входа в приложение  И реализует его логику. Запуск с параметрами дает следующий результат:</B>  <p> </p>
+ * <DD> <LI>Без параметров. Выведет список всех проектов(городов)     </p>   <p>
+ * <LI>(1 параметр, название проекта) выведет список всех рубрик в городе   </p>   <p>
+ * <LI>(2 параметра названия проекта и подрубрика) произведет поиск организаций в подрубрике  </p>   <p>
+ * <LI> (3 параметра названия проекта,  подрубрика и выходной файл) производит поиск и пишет результаты поиска * в  ввыходной файл в формате xls    </p> </OL>
+ */
 public class Main {
-    private static String projectList = "http://catalog.api.2gis.ru/project/list?";
-    private static String rubricURL = "http://catalog.api.2gis.ru/rubricator?where=";
-    private static String searchingRubricURL = "http://catalog.api.2gis.ru/searchinrubric?";
-    private static String versionKey = "version=1.3&key=ruxpxz1715";
+    /**
+     * Url для доступа к списку проектов  подробнее  <a href="в документации">http://api.2gis.ru/doc/firms/list/project-list/</a>
+     */
+    private static final String projectList = "http://catalog.api.2gis.ru/project/list?";
+    /**
+     * Url для доступа к списку рубрик     <a href="в документации">http://api.2gis.ru/doc/firms/list/rubricator/</a>
+     */
+    private static final String rubricURL = "http://catalog.api.2gis.ru/rubricator?where=";
+    /**
+     * Url для Поиска фирм по рубрикам     <a href="в документации">http://api.2gis.ru/doc/firms/searches/search/</a>
+     */
+    private static final String searchingRubricURL = "http://catalog.api.2gis.ru/search?";
+    /**
+     * Ключ доступа к API и версия API
+     */
+    private static final String versionKey = "version=1.3&key=ruxpxz1715";
 
+
+    /**
+     * Метод  создает InputStream для последующего преобразования в JSON
+     * Не выдает исключений. Если нет соединения, то напишет об этом в консоль и вернет null
+     *
+     * @param url адрес на страницу с JSON
+     * @return InputStream - входной поток, пригодный для приобразования в JSON
+     */
 
     public static InputStream getJSONData(String url) {
         DefaultHttpClient httpClient = new DefaultHttpClient();
@@ -37,28 +70,44 @@ public class Main {
         return data;
     }
 
-
+    /**
+     * Точка входа в приложение
+     *
+     * @param args = null Без параметров. Выведет список всех проектов(городов)     <p>
+     *             (1 параметр,название проекта) выведет список всех рубрик в городе     </p>   <p>
+     *             (2 параметра названия проекта и подрубрика) произведет поиск организаций в подрубрике     </p>   <p>
+     *             (3 параметра названия проекта,подрубрика и выходной файл) производит поиск и пишет результаты поиска * в  ввыходной файл в формате xls   </p>
+     */
     public static void main(String[] args) {
+
         System.out.println("Parser started..");
+
         if (args == null || args.length == 0) {
             printProjects();
         } else if (args.length == 1) {
             printRubrics(args[0]);
         } else if (args.length == 2) {
-            searchingRubric(args[0], args[1]);
+            searchingRubric(args[0], args[1], true);
         } else if (args.length == 3) {
-            exportXML(args[2], searchingRubric(args[0], args[1]));
+            exportXLS(args[2], args[1], searchingRubric(args[0], args[1], false));
         }
-
-
     }
 
-    private static void exportXML(String filepath, List<DoubleGISFirmtModel> models) {
+    /**
+     * Метод сохранения списка моделей в  XLS. По завершению работы сообщает в консоль об успехе либо выводит ошибку
+     *
+     * @param filepath Путь к файлу. Файл будет пересоздан
+     * @param bookName Названия книги внутри файла
+     * @param models   Список моделей.
+     */
+
+    private static void exportXLS(final String filepath, final String bookName, final List<DoubleGISFirmtModel> models) {
         HSSFWorkbook workbook = new HSSFWorkbook();
-        HSSFSheet sheet = workbook.createSheet("Firms");
+        HSSFSheet sheet = workbook.createSheet(bookName);
 
         int rownum = 0;
         for (DoubleGISFirmtModel model : models) {
+
             Row row = sheet.createRow(rownum++);
             int i = 0;
             Cell id = row.createCell(i++);
@@ -76,8 +125,6 @@ public class Main {
             Cell firmc = row.createCell(i++);
             firmc.setCellValue(model.getFirmscount());
         }
-
-
         try {
             FileOutputStream out =
                     new FileOutputStream(new File(filepath));
@@ -90,30 +137,66 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
+
+    /**
+     * Метод получения страницы запроса.
+     *
+     * @param projectName Название города
+     * @param rubric      Название рубрики или ключевого слова
+     * @param page        Номер страницы
+     * @return возвращает модель типа   SearchRubricsServerResponse, содержащую либо ошибку, либо данные со страницы
+     */
 
     private static SearchRubricsServerResponse searchingRubric(String projectName, String rubric, int page) {
         Reader r = new InputStreamReader(getJSONData(searchingRubricURL + "what=" + rubric + "&where=" + projectName + "&page=" + page + "&pagesize=50&sort=relevance&" + versionKey));
 
-//        System.out.println(searchingRubricURL + "what=" + rubric + "&where=" + projectName + "&page=" + page + "&pagesize=30&sort=relevance&" + versionKey);
+        System.out.println(searchingRubricURL + "what=" + rubric + "&where=" + projectName + "&page=" + page + "&pagesize=50&sort=relevance&" + versionKey);
 
         Gson gson = new Gson();
         return gson.fromJson(r, SearchRubricsServerResponse.class);
     }
 
-    private static List<DoubleGISFirmtModel> searchingRubric(String projectName, String rubric) {
+    /**
+     * Метод поиска по рубрике или ключевому слову. Метод делает задержку потока, если время между загрузками менее 1 секунды Это требуется для соблюдения правил использования демонстрационного ключа
+     *
+     * @param projectName Название города
+     * @param rubric      Название рубрики или ключевого слова
+     * @param print       параметр отвечает - печатать или нет в лог лист данных
+     * @return позвращается список моделей фирм
+     */
+
+    private static List<DoubleGISFirmtModel> searchingRubric(String projectName, String rubric, boolean print) {
         try {
             List<DoubleGISFirmtModel> models = new ArrayList<DoubleGISFirmtModel>();
             SearchRubricsServerResponse serverResponse = null;
             int page = 1;
             do {
-                System.out.println("page " + page);
+                long startTime = System.currentTimeMillis();
+
                 serverResponse = searchingRubric(projectName, rubric, page);
+
+                System.out.println(serverResponse);
+                System.out.println("page " + page + " is get");
+
+                if (print) {
+                    for (DoubleGISFirmtModel result : serverResponse.getResult()) {
+                        System.out.println(result);
+                    }
+                }
                 models.addAll(serverResponse.getResult());
                 page++;
-            } while ((serverResponse.getTotal() > models.size()) || (serverResponse.error_code != null));
 
+                long endTime = System.currentTimeMillis();
+                long dif = endTime - startTime;
+                if (dif < 1000) {
+                    try {
+                        System.out.println("sleep " + (1000 - dif));
+                        Thread.sleep(1000 - dif);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            } while ((serverResponse.getTotal() > models.size()) || (serverResponse.getError_code() != null));
 
             return models;
         } catch (Exception ex) {
@@ -122,6 +205,12 @@ public class Main {
         }
 
     }
+
+    /**
+     * Метод    получает и выводит список всех рубрик в городе
+     *
+     * @param projectName Название города
+     */
 
     private static void printRubrics(String projectName) {
         try {
@@ -153,6 +242,9 @@ public class Main {
         }
     }
 
+    /**
+     * Метод    получает и выводит список всех городов в системе
+     */
     private static void printProjects() {
         try {
             Gson gson = new Gson();
@@ -168,9 +260,9 @@ public class Main {
 
                 if (serverResponse.getError_code() == null) {
 
-                    System.out.println("" + serverResponse.getResult().size());
+                    System.out.println("" + serverResponse.getProjectModelResult().size());
 
-                    for (DoubleGISProjectModel model : serverResponse.getResult()) {
+                    for (DoubleGISProjectModel model : serverResponse.getProjectModelResult()) {
 
                         System.out.println(model);
                     }
